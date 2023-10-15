c4497646e9d26406423533148d6befdb718149cb
Semjon Nirmann <semjon.nirmann@yahoo.com>
2023-10-15 22:41:30 +0200

    ...

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import styles from './App.module.css';
+import useAdviceAPI from './useAdviceApi';
+
+const App = () => {
+  const { currentAdvice, getAdvice } = useAdviceAPI();
+  const { id, advice } = currentAdvice || {};
+
+  return (
+    <div className={styles.wrapper}>
+      <h1>Advice #{id}</h1>
+      {!advice ? <p>...</p> : <blockquote>{advice}</blockquote>}
+      <picture className={styles.separator}>
+        <source media='(min-width: 550px)' srcSet='src/assets/images/pattern-divider-desktop.svg'></source>
+        <img src='src/assets/images/pattern-divider-mobile.svg' alt='Horizontal rules image'></img>
+      </picture>
+      <button className={styles.bubble} onClick={getAdvice}></button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/useAdviceAPI.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const useAdviceAPI = () => {
+  const baseUrl = 'https://api.adviceslip.com/advice';
+  const [currentAdvice, setCurrentAdvice] = useState(null);
+
+  const getAdvice = async () => {
+    const response = await axios.get(baseUrl);
+    setCurrentAdvice(response.data.slip);
+  };
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return { currentAdvice, getAdvice };
+};
+
+export default useAdviceAPI;
